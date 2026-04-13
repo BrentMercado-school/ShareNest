458628a6ecad4ae4718bbcbc10ae3ab3172bfaf8
Kielo Bash Mercado <kielobashmercado@Kielos-Mac-mini.local>
2026-04-13 13:40:39 +0800

    added ui integration

---
+++ ShareNestUi/login.js
@@ -0,0 +1,36 @@
+const loginForm = document.getElementById("loginForm")
+const API_URL = "http://127.0.0.1:8000/api/"
+
+if (loginForm) {
+    loginForm.addEventListener("submit", async (e) => {
+        e.preventDefault()
+
+        const name = document.getElementById("name").value
+        const password = document.getElementById("password").value
+
+        try {
+            const response = await fetch(API_URL + "users/login/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+                body: JSON.stringify({
+                    name,
+                    password
+                })
+            })
+
+            const data = await response.json();
+
+            if (response.ok) {
+                window.location.href = "http://127.0.0.1:5500/ShareNest/ShareNestUi/dashboard.html"
+            } else {
+                alert(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            alert("Something went wrong.");
+        }
+    })
+}
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
-from api.views import get_health_check, CategoryListAPIView, UserListAPIView, UserCreateAPIView, ItemListAPIView, \
-    RegisterUserAPIView
+from api.views import (
+    CategoryListAPIView,
+    CurrentUserAPIView,
+    ItemListAPIView,
+    LoginUserAPIView,
+    RegisterUserAPIView,
+    UserCreateAPIView,
+    UserListAPIView,
+    get_health_check,
+)
 
 # TODO: 5 for routing
 urlpatterns = [
@@ -9,5 +17,8 @@ urlpatterns = [
     path("categories/", CategoryListAPIView.as_view(), name="category-list"),
     path("users/", UserListAPIView.as_view(), name="user-list"),
     path("users/create/", UserCreateAPIView.as_view(), name="user-create"),
-
+    path("items/", ItemListAPIView.as_view(), name="item-list"),
+    path("users/register/", RegisterUserAPIView.as_view(), name="register"),
+    path("users/login/", LoginUserAPIView.as_view(), name="login"),
+    path("users/me/", CurrentUserAPIView.as_view(), name="login"),
 ]
