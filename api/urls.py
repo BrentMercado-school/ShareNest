8ac3189dc1d475f00e7e8bd90bca14a7fb6ca760
BrentMercado-school <brentallen.mercado@benilde.edu.com>
2026-04-13 20:25:46 +0800

    feat: add get items

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
-from api.views import get_health_check, CategoryListAPIView, UserListAPIView, UserCreateAPIView, ItemListAPIView, \
-    RegisterUserAPIView
+from api.views import (
+    CategoryListAPIView,
+    CurrentUserAPIView,
+    ItemListAPIView,
+    LoginUserAPIView,
+    RegisterUserAPIView,
+    UserCreateAPIView,
+    UserItemsAPIView,
+    UserListAPIView,
+    get_health_check,
+)
 
 # TODO: 5 for routing
 urlpatterns = [
@@ -9,5 +18,9 @@ urlpatterns = [
     path("categories/", CategoryListAPIView.as_view(), name="category-list"),
     path("users/", UserListAPIView.as_view(), name="user-list"),
     path("users/create/", UserCreateAPIView.as_view(), name="user-create"),
-
+    path("items/", ItemListAPIView.as_view(), name="item-list"),
+    path("users/register/", RegisterUserAPIView.as_view(), name="register"),
+    path("users/login/", LoginUserAPIView.as_view(), name="login"),
+    path("users/me/", CurrentUserAPIView.as_view(), name="login"),
+    path("users/owned-items/", UserItemsAPIView.as_view(), name="user-item-list"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,44 +1,49 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from django.http import JsonResponse
 from rest_framework.parsers import MultiPartParser, JSONParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.models import Category, User, Item
-from api.serializers import CategorySerializer, UserSerializer, ItemSerializer, RegisterUserSerializer, \
-    LoginUserSerializer
+from api.serializers import (
+    CategorySerializer,
+    UserSerializer,
+    ItemSerializer,
+    RegisterUserSerializer,
+    LoginUserSerializer,
+)
 
 
 # TODO: 4 for controlling what data the API returns
 def get_health_check(request):
-    return JsonResponse(
-        {
-            "status": "ok",
-            "message": "API is running"
-        },
-        status=200
-    )
+    return JsonResponse({"status": "ok", "message": "API is running"}, status=200)
+
 
 class CategoryListAPIView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class UserListAPIView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class ItemListAPIView(generics.ListAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
 
 class RegisterUserAPIView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterUserSerializer
     parser_classes = [MultiPartParser, FormParser, JSONParser]
+
 
 class LoginUserAPIView(generics.GenericAPIView):
     serializer_class = LoginUserSerializer
@@ -48,11 +53,41 @@ class LoginUserAPIView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
 
         user = UserSerializer(serializer.validated_data["user"]).data
-        return Response({
-            "user": {
-                "id": user.get('id'),
-                "name": user.get('name'),
-                "email": user.get('email'),
-            },
-            "message": "Login Successful",
-        })
+
+        request.session["user_id"] = user.get("id")
+
+        return Response(
+            {
+                "user": {
+                    "id": user.get("id"),
+                    "name": user.get("name"),
+                    "email": user.get("email"),
+                },
+                "message": "Login Successful",
+            }
+        )
+
+
+class CurrentUserAPIView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+
+        userId = request.session.get("user_id")
+
+        try:
+            currentUser = User.objects.get(id=userId)
+        except User.DoesNotExist:
+            return Response(
+                {"detail": "User not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(UserSerializer(currentUser).data)
+
+
+class UserItemsAPIView(generics.ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        userId = self.request.session.get("user_id")
+        return Item.objects.filter(owner=userId)
